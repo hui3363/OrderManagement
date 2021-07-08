dd444e921f987bde8551aaf4e93d1a4e919e2eb1
hui3363 <hui3363@gmail.com>
2021-07-08 08:21:54 +0000

    ver1

--- src/main/java/com/github/prgrms/orders/OrderRestController.java
+++ src/main/java/com/github/prgrms/orders/OrderRestController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.prgrms.errors.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import static com.github.prgrms.utils.ApiUtils.ApiResult;
 import static com.github.prgrms.utils.ApiUtils.success;
@@ -19,29 +21,33 @@ import static java.util.stream.Collectors.toList;
 public class OrderRestController {
   // TODO findAll, findById, accept, reject, shipping, complete 메소드 구현이 필요합니다.
 
-  private final OrderService orderService;
-
-  public OrderRestController(OrderService orderService) {
-    this.orderService = orderService;
+  @GetMapping("{id}")
+  public void findById(@PathVariable Long id) {
+    System.out.println("order search");
   }
-
-  @GetMapping(path = "{id}")
-  public ApiResult<OrderDto> findById(@PathVariable Long id) {
-    return success(
-      orderService.findById(id)
-        .map(OrderDto::new)
-        .orElseThrow(() -> new NotFoundException("Could nof found user for " + id))
-    );
-  }
-
 
   @GetMapping
-  public ApiResult<List<OrderDto>> findAll() {
-    /*return productService.findAll().stream()
-      .map(ProductDto::new)
-      .collect(toList());*/
-    return success(orderService.findAll().stream().map(OrderDto::new).collect(toList()));
+  public void findAll(){
+    System.out.println("order All");
   }
 
+  
+  @PatchMapping("{id}/accept")
+  public void accept(@PathVariable Long id){
+    System.out.println("accept");
+  }
+  @PatchMapping("{id}/shipping")
+  public void shipping(@PathVariable Long id){
+    System.out.println("shipping");
+  }
+  @PatchMapping("{id}/complete")
+  public void complete(@PathVariable Long id){
+    System.out.println("complete");
+  }
+  
+  @PatchMapping("{id}/reject")
+  public void reject(@PathVariable Long id){
+    System.out.println("reject");
+  }
 
 }
