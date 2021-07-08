6a84f3ce501991c5fef68c1e6df849c2f5aa7e6d
hui3363 <hui3363@gmail.com>
2021-07-08 11:16:22 +0000

    ver1

--- src/main/java/com/github/prgrms/orders/ReviewRestController.java
+++ src/main/java/com/github/prgrms/orders/ReviewRestController.java
@@ -6,22 +6,25 @@ import com.github.prgrms.security.JwtAuthentication;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import static com.github.prgrms.utils.ApiUtils.ApiResult;
+import static com.github.prgrms.utils.ApiUtils.success;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("api/orders")
 public class ReviewRestController {
   // TODO review 메소드 구현이 필요합니다.
-
-  
-  @PostMapping("{id}/review")
-  public void review(@PathVariable Long id){
-    System.out.println("review");
-  }
 
   
 }
